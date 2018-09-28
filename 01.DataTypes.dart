2b37f14cd23bb390e0bbd899e683c93db857b496
Amin Abdisamad <theaminux@gmail.com>
2018-09-28 20:04:26 +0300

    Numbers 1- int 2. double

--- 01.DataTypes.dart
+++ 01.DataTypes.dart
@@ -1,7 +1,25 @@
+import 'dart:math';
+
+/* 
+This section contains : 
+1. Numbers - Int and double 
+2. Strings
+3.Booleans (bool)
+*/
 void main() {
-// Numbers
+// Numbers - there are two types of numbers 1. int 2. double
+  // Int
+  int x, y, z;
+  x = 10;
+  y = 20;
+  z = x + y;
+  print(z);
+  // Double
+  double a, b, c;
+  a = 2.0;
+  b = 10.0;
+  c = pow(a, b);
+  print(c);
 // Strings
 // Booleans
-// Lists
-// Maps
 }
