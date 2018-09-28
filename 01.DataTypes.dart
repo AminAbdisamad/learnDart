895dd6f91bbf4cb334dce3495876abdad157671c
Amin Abdisamad <theaminux@gmail.com>
2018-09-28 20:07:20 +0300

    Numbers

--- 01.DataTypes.dart
+++ 01.DataTypes.dart
@@ -8,12 +8,19 @@ This section contains :
 */
 void main() {
 // Numbers - there are two types of numbers 1. int 2. double
+  num ab = 2;
+  num ac = 12.5;
+  num az;
+  az = ab + ac;
+  print(az);
+
   // Int
   int x, y, z;
   x = 10;
   y = 20;
   z = x + y;
   print(z);
+
   // Double
   double a, b, c;
   a = 2.0;
